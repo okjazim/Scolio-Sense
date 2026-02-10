f407ab14708c6a0b3f47a98bb316b95611d6feb0
okjazim <jazimjunaith02@gmail.com>
2026-02-10 19:20:00 +0100

    added cred example

---
+++ cred_example.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi credentials
+const char* WIFI_SSID = "YOUR_SSID";
+const char* WIFI_PASSWORD = "YOUR_PASSWORD";
+
+// Adafruit IO
+const char* mqtt_server = "io.adafruit.com";
+const int mqtt_port = 1883;
+const char* MQTT_USER = "YOUR_ADAFRUIT_IO_USERNAME";
+const char* MQTT_PASS = "YOUR_ADAFRUIT_IO_KEY";
+
+#endif
